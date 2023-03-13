d4fad12982906f1b16741c607bb502b62b1442cc
SebiC98 <sebastiancioata98@gmail.com>
2023-03-13 20:32:08 +0200

    Basic Rock Paper Scissors game

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -34,6 +34,7 @@ struct ContentView: View {
                 }
             }
         }
+        .font(.largeTitle)
         .alert("Game over!", isPresented: $showAlert){
             Button("Restart", action: restartGame)
         } message: {
